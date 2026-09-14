1383a46e4e2382620169af014cd09da8a1b1fc2a
Daniel Marshall <dpm42@duke.edu>
2026-09-14 10:03:33 -0400

    style: pre-commit conformity

--- src/nd_line/nd_line.py
+++ src/nd_line/nd_line.py
@@ -4,7 +4,6 @@ Copyright Daniel Marshall
 """
 
 import math
-from typing import Optional
 
 import numpy as np
 from numpy import ndarray
@@ -72,7 +71,7 @@ class nd_line:
         assert ratio <= 1, "Ratio for interp_rat() must be a value from 0 to 1"
         return self.interp(ratio * self.length)
 
-    def splineify(self, samples: Optional[int] = None, s: float = 0) -> None:
+    def splineify(self, samples: int | None = None, s: float = 0) -> None:
         """Turn line into a spline approximation, currently occurs in place.
 
         :param samples: number of samples to use for spline approximation
